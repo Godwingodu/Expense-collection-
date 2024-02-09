94a2198b8a4171447b3751662a6e4c1f41c77430
Godwingodu <godwinvinson69@gmail.com>
2024-02-10 01:23:11 +0530

    first commit

---
+++ GoogleSheet.js
@@ -0,0 +1,12 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwWHh7hyJF-Xlkzn84otOEn8hsHin8tuLqu_twgQAyBgUq-PKXKB2kq6bAUb_6tbL5K/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  console.log("submitted")
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
